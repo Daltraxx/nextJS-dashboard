2842fc2ef336087d687bf1a90c85d9643cc30900
Dalton Pettus <daltpettus@gmail.com>
2025-05-23 14:54:59 -0700

    use loading skeleton for dashboard and place loading.tsx and page.tsx in (overview) route group so /dashboard/customers and /dashboard/invoices are unaffected

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -2,6 +2,9 @@
 // Since <SideNav> is static, it's shown immediately. The user can interact with <SideNav> while the dynamic content is loading.
 // The user doesn't have to wait for the page to finish loading before navigating away (this is called interruptable navigation).
 
+import DashboardSkeleton from "../../ui/skeletons"; // A loading skeleton is a simplified version of the UI. Many websites use them as a placeholder (or fallback) to indicate to users that the content is loading.
+
 export default function Loading() {
-    return <div>Loading...</div>;
+    //  Any UI you add in loading.tsx will be embedded as part of the static file, and sent first. Then, the rest of the dynamic content will be streamed from the server to the client.
+    return <DashboardSkeleton />;
 }
--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,16 @@
+// Route groups allow you to organize files into logical groups without affecting the URL path structure. 
+// When you create a new folder using parentheses (), the name won't be included in the URL path. 
+// So /dashboard/(overview)/page.tsx becomes /dashboard.
+// Here, you're using a route group to ensure loading.tsx only applies to your dashboard overview page. 
+// However, you can also use route groups to separate your application into sections 
+// (e.g. (marketing) routes and (shop) routes) or by teams for larger applications.
+
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
  
 export default async function Page() {
     const revenue = await fetchRevenue();
