8094a6bcc32ccb8e114074630164294c058a8d4e
Dalton Pettus <daltpettus@gmail.com>
2025-05-24 16:31:36 -0700

    create actions.ts for Server Actions and add createInvoice function

---
+++ app/lib/actions.ts
@@ -0,0 +1,6 @@
+// By adding the 'use server', you mark all the exported functions within the file as Server Actions. 
+// These server functions can then be imported and used in Client and Server components. 
+// Any functions included in this file that are not used will be automatically removed from the final application bundle.
+'use server';
+
+export async function createInvoice(formData: FormData) {}
