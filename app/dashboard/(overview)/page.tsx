9ce7f4debcfdebc0fc0689bbe65d6a5b6c9ee12f
Dalton Pettus <daltpettus@gmail.com>
2025-05-23 19:26:37 -0700

    move fetchRevenue inside RevenueChart component and wrap RevenueChart in Suspense component with RevenueChartSkeleton as a fallback while data is fetched

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,10 +10,12 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
+import {fetchLatestInvoices, fetchCardData } from '../../lib/data';
+
+import { Suspense } from 'react'; // used to wrap RevenueChart and provide fallback while it fetches slow-loading data inside the component, allowing rest of page to load without slow component blocking it
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-    const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
     return (
@@ -28,7 +30,9 @@ export default async function Page() {
             <Card title="Total Customers" value={numberOfCustomers} type="customers" />
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-            <RevenueChart revenue={revenue}  />
+            <Suspense fallback={<RevenueChartSkeleton />}>
+                <RevenueChart />
+            </Suspense>
             <LatestInvoices latestInvoices={latestInvoices} />
         </div>
         </main>
