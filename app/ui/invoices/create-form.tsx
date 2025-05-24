f80090b241a682cc43586b72b232349380086437
Dalton Pettus <daltpettus@gmail.com>
2025-05-24 16:35:58 -0700

    import createInvoice in create-form and use as form action prop

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}> {/*in React, the action attribute is considered a special prop. Behind the scenes, Server Actions create a POST API endpoint.*/}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
