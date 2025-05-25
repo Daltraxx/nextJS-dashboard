a271f12c55ce74e2b15e22323e3ad84af2295595
Dalton Pettus <daltpettus@gmail.com>
2025-05-24 19:06:31 -0700

    add dynamic route for /dashboard/invoices/[id]/edit, update link on UpdateInvoice to take user to correct edit page, and prepopulate edit form with correct invoice data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+//By wrapping [id] in square brackets within the /invoices folder, we have created a dynamic route segment (/dashboard/invoices/[id]/edit)
+
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page(props: { params: Promise<{ id: string }> }) { // Page components can accept a prop called params which you can use to access the id
+   const params = await props.params;
+   const id = params.id;
+
+   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+   return (
+      <main>
+         <Breadcrumbs
+         breadcrumbs={[
+            { label: 'Invoices', href: '/dashboard/invoices' },
+            {
+               label: 'Edit Invoice',
+               href: `/dashboard/invoices/${id}/edit`,
+               active: true,
+            },
+         ]}
+         />
+         <Form invoice={invoice} customers={customers} />
+      </main>
+   );
+}
