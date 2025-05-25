9631370a8ed4a1f47f4ef511489dd06b3eaef7a6
Dalton Pettus <daltpettus@gmail.com>
2025-05-25 10:18:35 -0700

    add error.tsx page for handling exceptions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -76,6 +76,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+   // throw new Error('Failed to Delete Invoice'); use to simulate an uncaught exception, which will trigger the error.tsx file in /dashboard/invoices
+
    try {
       await sql `DELETE FROM invoices WHERE id = ${id}`;
    } catch (error) {
