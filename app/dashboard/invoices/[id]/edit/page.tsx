c13de41ef51226f888f2206d9ee3191f3d99870a
Dalton Pettus <daltpettus@gmail.com>
2025-05-25 10:26:53 -0700

    add not-found.tsx and use for handling attempted edits to non-existing invoices

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,12 +3,18 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: { params: Promise<{ id: string }> }) { // Page components can accept a prop called params which you can use to access the id
    const params = await props.params;
    const id = params.id;
 
    const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+   //if invoice not found, trigger not-found.tsx
+   if (!invoice) {
+      notFound();
+   }
 
    return (
       <main>
